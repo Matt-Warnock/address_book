2b2ce4d162a252b750b6dfb815756287b2cd13fa
MattW <drflamerock@yahoo.co.uk>
2020-04-21 14:19:03 +0100

    Add delete and error message logic fix error message casscadeing issue

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-function rowToDelete() {
-  const userEntry = document.getElementById('user_contact_name');
+
+function deleteRow(toBeDeleted, array) {
+  const note = document.getElementById('error_message');
+
+  if (toBeDeleted === undefined) {
+    note.textContent = 'This contact doesnt exist';
+  } else {
+    note.textContent = '';
+    toBeDeleted.remove();
+  }
+
+}
+
+
+function contactDatabaseDeletion() {
   const allAddress = document.getElementById('contact_details');
+  const userEntry = document.getElementById('user_contact_name');
 
   let rows = Array.from(allAddress.rows);
+  let rowIndex = rows.find(row => row.cells[0].textContent === userEntry.value);
 
-  return rows.find(row => row.cells[0].textContent === userEntry.value);
+  deleteRow(rowIndex, rows);
 }
+
 
 
 document.getElementById('user_contact_name').addEventListener('keydown', event => {
 
   if(event.code === 'Enter'){
 
-  rowToDelete();
+  contactDatabaseDeletion();
 
   }
 });
