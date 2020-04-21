cda3bd8d5ff1b64fd0ce3c768ee9ed4d964c5b13
MattW <drflamerock@yahoo.co.uk>
2020-04-21 15:47:34 +0100

    Refactor to seperate concerns

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 
-function deleteRow(toBeDeleted, array) {
+function buildContactList() {
+  const allAddress = document.getElementById('contact_details');
+  return Array.from(allAddress.rows);
+}
+
+function infoMessage(toBeDeleted) {
   const note = document.getElementById('error_message');
 
-  if (toBeDeleted === undefined) {
+    if(toBeDeleted === undefined) {
     note.textContent = 'This contact doesnt exist';
-  } else {
+  }else {
     note.textContent = '';
-    toBeDeleted.remove();
   }
-
 }
 
-
-function contactDatabaseDeletion() {
-  const allAddress = document.getElementById('contact_details');
-  const userEntry = document.getElementById('user_contact_name');
-
-  let rows = Array.from(allAddress.rows);
-  let rowIndex = rows.find(row => row.cells[0].textContent === userEntry.value);
-
-  deleteRow(rowIndex, rows);
+function deleteContact(toBeDeleted) {
+    if(toBeDeleted !== undefined) {
+      toBeDeleted.remove();
+  }
 }
 
+function contactDatabaseDeletion(nameInput) {
+  let rows = buildContactList();
+
+  let rowIndex = rows.find(row => row.cells[0].textContent === nameInput.value);
+
+  infoMessage(rowIndex);
+  deleteContact(rowIndex);
+}
 
 
 document.getElementById('user_contact_name').addEventListener('keydown', event => {
+  const userEntry = document.getElementById('user_contact_name');
 
   if(event.code === 'Enter'){
-
-  contactDatabaseDeletion();
-
+    contactDatabaseDeletion(userEntry);
   }
 });
