8da9470bf249c4c84f97b34561af514e17423da7
MattW <drflamerock@yahoo.co.uk>
2020-04-24 18:38:02 +0100

    Add row sorting aphabeticly in ascending order

--- sort.js
+++ sort.js
@@ -1,4 +1,21 @@
 document.getElementById('column_names').addEventListener('click', event => {
   let changeThisColomnIndex = event.target.cellIndex;
-
+  console.log(columndescent(changeThisColomnIndex));
 });
+
+function columndescent(userSelected) {
+  let rows = buildContactList();
+  let colunmClicked = userSelected;
+
+  return rows.sort((a, b) => {
+    let firstCell = a.cells[colunmClicked].textContent;
+    let nextCell = b.cells[colunmClicked].textContent;
+    if(firstCell < nextCell){
+      return -1;
+    }
+    if(firstCell > nextCell){
+      return 1;
+    }
+    return 0;
+  });
+}
