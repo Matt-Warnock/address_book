a64b72bac0962f94951ac86ade580b72f4c1493d
MattW <drflamerock@yahoo.co.uk>
2020-05-11 13:27:47 +0100

    Convert detete javascript to objects

--- delete.js
+++ delete.js
@@ -1,33 +1,53 @@
-function buildContactList() {
-  const allAddress = document.getElementById('contact_details');
-  return Array.from(allAddress.rows);
-}
+const addressBook = {
+  allAddress: document.getElementById('contact_details'),
 
-function displayInfoMessage(userResult) {
-  const note = document.getElementById('error_message');
-  note.textContent = userResult;
-}
+  buildContactList() {
+    return Array.from(this.allAddress.rows);
+  },
 
-function deleteContact(toBeDeleted) {
-  toBeDeleted.remove();
-}
+  findContactToDelete(nameInput) {
+    let rows = this.buildContactList();
+    return rows.find(row => row.cells[0].textContent === nameInput);
+  },
 
-function contactDatabaseDeletion(nameInput) {
-  let rows = buildContactList();
-  let addressToDelete = rows.find(row => row.cells[0].textContent === nameInput.value);
-
-  if(addressToDelete !== undefined){
-    displayInfoMessage('');
-    deleteContact(addressToDelete);
-  }else {
-    displayInfoMessage('That contact does not exist');
+  deleteContact(toBeDeleted) {
+    toBeDeleted.remove();
   }
-}
+};
 
-document.getElementById('user_contact_name').addEventListener('keydown', event => {
-  const userEntry = document.getElementById('user_contact_name');
+const ui = {
+  _userEntry: document.getElementById('user_contact_name'),
+  _messageOutput: document.getElementById('error_message'),
 
-  if(event.code === 'Enter'){
-    contactDatabaseDeletion(userEntry);
+  get userEntry() {
+    return this._userEntry.value;
+  },
+
+  set displayInfoMessage(userResult) {
+    if (typeof userResult === 'string') {
+      this._messageOutput.textContent = userResult;
+    }else {
+      console.error('data type should be string!');
+    }
+  },
+
+  _deletionProcedure() {
+    let addressToDelete = addressBook.findContactToDelete(this.userEntry);
+
+    if(addressToDelete !== undefined){
+      this.displayInfoMessage = '';
+      addressBook.deleteContact(addressToDelete);
+    }else {
+      this.displayInfoMessage = 'That contact does not exist';
+    }
+  },
+
+  initiaize() {
+    document.getElementById('user_contact_name').addEventListener('keydown', event => {
+      if(event.code === 'Enter'){
+        this._deletionProcedure();
+      }
+    });
   }
-});
+};
+ui.initiaize();
