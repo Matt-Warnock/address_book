972a1f2e19bccb74bf90682cdc4958d2ffe4e30c
MattW <drflamerock@yahoo.co.uk>
2020-05-15 13:32:50 +0100

    convert main.js to classes

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const addressBook = {
-  allAddress: document.getElementById('contact_details'),
-
+class AddressBook {
+  constuctor() {
+  this.allAddress = document.getElementById('contact_details');
+}
   buildContactList() {
     return Array.from(this.allAddress.rows);
   }
-};
+}
 
-const mainUi = {
-    _messageOutput: document.getElementById('error_message'),
-
+class MainUi {
+  constuctor() {
+    this._messageOutput = document.getElementById('error_message');
+}
     set displayInfoMessage(userResult) {
         this._messageOutput.textContent = userResult;
     }
-};
+}
+const addressBook = new AddressBook();
+const mainUi = new MainUi();
